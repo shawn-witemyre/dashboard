a2beda4a05ffdfb4077dc74be5359e5c5370cdd3
Shawn Witemyre <switemyre@comcast.net>
2025-05-14 19:30:42 -0400

    Added html, css, and javascript to fetch image

---
+++ index.js
@@ -0,0 +1,7 @@
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url('${data.urls.full}')`;
+  });
